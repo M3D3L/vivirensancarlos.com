22f42516fbe54db3175bd942881a75806cc8fc95
M3D3L <72050286+M3D3L@users.noreply.github.com>
2026-01-18 05:42:26 +0000

    Deploying to gh-pages from @ M3D3L/common@45d5cde21be80be142a42750b0bf79b524067841 🚀

---
+++ _nuxt/B4LB5NSQ.js
@@ -0,0 +1,1 @@
+import{c as a}from"./CwEYHAzV.js";import{e as r,c as t,o as n,l as c,i as l,I as p}from"#entry";const _=r({__name:"CardFooter",props:{class:{}},setup(e){const s=e;return(o,m)=>(n(),t("div",{class:c(l(a)("flex items-center p-6 pt-0",s.class))},[p(o.$slots,"default")],2))}});export{_};
